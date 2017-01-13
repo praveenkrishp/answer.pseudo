604d2d218afb6706e2078d9e9b57fe96cc8f89ec
praveenkrishp <praveenkrishp@gmail.com>
2017-01-13 23:27:02 +0530

    Name and random number
    i/p: name
    o*p: name and random number between 6 and 15

---
+++ program.js
@@ -0,0 +1,14 @@
+function getOutput(nm) //function declaration, formal parameter
+{
+  var name = nm; //saving parameter to a local variable  function(result)
+  var number;
+  function getRandon(callback) // asynchronous function to get random number
+  {
+      var num = Math.random() * (15 - 6) + 6;  //getting random number between 6 and 15
+  }; 
+  getRandom(function(result) //function call with wait
+  {
+    number=result;  //result obtained
+    return {data : name+' '+num}; //return in json format
+  });
+};
